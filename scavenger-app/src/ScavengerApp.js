273ea1acc49e9f245b2a1b7bd11de5e29bb936c3
ASnailman <harriskhoo21@gmail.com>
2025-02-28 18:43:12 -0600

    base code

---
+++ scavenger-app/src/ScavengerApp.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+// import './App.css';
+
+function ScavengerApp() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default ScavengerApp;
